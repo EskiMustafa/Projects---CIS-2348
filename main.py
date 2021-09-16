66482e958acccc7cd23c19f2a30b844f65515e76
EskiMustafa <89745927+EskiMustafa@users.noreply.github.com>
2021-09-16 15:14:37 -0500

    Add files via upload
    Zylabs 1.20

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# Mustafa Shabbir Eski
+#Mustafa Shabbir Eski
 # PSID: 2046388
-#Zylabs 1.12
-userNum = int(input())
-userNumSquared = userNum ** 2
+# Zylabs 1.20
 
-print(userNumSquared, end='\n')
+number = int(input("Enter integer:\n"))
+print ("You entered:", number)
+print (number,'squared is',number**2)
+print ('And',number, 'cubed is',number**3, '!!')
+
+number2 = int(input("Enter another integer:\n"))
+print (number,'+',number2,'is', number+number2)
+print (number,'*',number2,'is', number*number2)
