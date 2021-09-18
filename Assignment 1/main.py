353b48548e794af030eab446766575fe839d92c2
EskiMustafa <89745927+EskiMustafa@users.noreply.github.com>
2021-09-17 19:52:38 -0500

    Add files via upload

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,1 +1,12 @@
+#Mustafa Shabbir Eski
+# PSID: 2046388
+# Zylabs 1.20
 
+number = int(input("Enter integer:\n"))
+print ("You entered:", number)
+print (number,'squared is',number**2)
+print ('And',number, 'cubed is',number**3, '!!')
+
+number2 = int(input("Enter another integer:\n"))
+print (number,'+',number2,'is', number+number2)
+print (number,'*',number2,'is', number*number2)
