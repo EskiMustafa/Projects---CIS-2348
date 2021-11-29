c646d36dbd475e6751666a3778dad94da0b5041f
EskiMustafa <89745927+EskiMustafa@users.noreply.github.com>
2021-11-29 17:29:41 -0600

    Add files via upload

---
+++ Assignment 4/Fat Burn.py
@@ -0,0 +1,21 @@
+#Mustafa Eski
+# PSID: 2046388
+
+def get_age():
+    age = int(input())
+    if age <= 18 or age >= 75:
+        raise ValueError("Invalid age.")
+    else:
+        return age
+
+def fat_burning_heart_rate(age):
+    return (220 - age) * 0.7
+
+
+if __name__ == "__main__":
+    try:
+        age = get_age()
+        print("Fat burning heart rate for a", age, "year-old:", fat_burning_heart_rate(age), "bpm")
+    except ValueError as e:
+        print(e)
+        print("Could not calculate heart rate info.\n")
